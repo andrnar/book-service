5eed1950986647758b24d72c537922413df0c81c
Madis Salumae <madis.salumae@icefire.ee>
2020-12-16 14:33:36 +0200

    initial commit

---
+++ src/main/java/bookapp/controller/BookController.java
@@ -0,0 +1,17 @@
+package bookapp.controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@RestController
+@EnableWebMvc
+public class BookController {
+    private static final Book[] books = {
+            new Book(1L, "Nemesis", "Isaac Asimov"),
+            new Book(2L, "Great Expectations", "Charles Dickens"),
+            new Book(3L, "The Chronicles of Narnia", "C.S. Lewis")
+    };
+    @RequestMapping(path = "/books", method = RequestMethod.GET)
+    public Book[] books() {
+        return books;
+    }
+}
